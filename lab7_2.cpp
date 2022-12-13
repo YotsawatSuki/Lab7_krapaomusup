79d25bfc4fae40dac6c46eaffdec8ac24053c01a
Yotsawat Sukinee <yotsawat2557@gmail.com>
2022-12-13 16:16:04 +0700

    finish lab7_2

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,12 +3,30 @@
 using namespace std;
 
 int main(){
-	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k;
+	cout << "Please input k: ";
+	cin >> k;
 	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
-	}
+			P[i] = 2*i+1;
+			i++;
+		}
 	
+	if(k > 0){
+		int i = 0;
+		int result = 0;
+		while(i < 5000){
+			if(P[i]%k == 0){
+				result = result - P[i];
+			}else{
+				result = result + P[i];
+			}
+			i++;
+		}
+			
+		cout << "Result = " << result;
+
+	}else{
+		cout << "Invalid input!!!";
+	}
 	return 0;
 }
